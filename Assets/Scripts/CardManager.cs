0ee95dfb3e9e9ab210a18c36f48cffd81675b2cb
Vanessa <vn4261@rit.edu>
2025-02-22 23:26:55 -0500

    Adding cards randomized

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -4,12 +4,21 @@ using DG.Tweening;
 
 public class CardManager : MonoBehaviour
 {
-    public GameObject cardPrefab;
-    public Transform[] cardSlots;
-    private List<GameObject> currentCards = new List<GameObject>();
+    public GameObject cardPrefab; // Prefab for the card
+    public Transform[] cardSlots; // Array of slots where cards will be spawned
+    private List<GameObject> currentCards = new List<GameObject>(); // List of currently active cards
+    private Sprite[] cardSprites; // Array to hold all available card sprites
 
     void Start()
     {
+        // Load all sprites from the "Cards" folder in Prefabs/Resources
+        cardSprites = Resources.LoadAll<Sprite>("Cards");
+
+        if (cardSprites.Length == 0)
+        {
+            Debug.LogError("No sprites found in the 'Cards' folder. Please add some sprites.");
+        }
+
         DrawInitialCards();
     }
 
@@ -23,10 +32,24 @@ public class CardManager : MonoBehaviour
 
     void SpawnRandomCard(Transform slot)
     {
+        // Instantiate a new card at the given slot position
         GameObject newCard = Instantiate(cardPrefab, slot.position, Quaternion.identity);
 
-        AnimateIdle(newCard.transform);
+        // Get the SpriteRenderer component of the card
+        SpriteRenderer spriteRenderer = newCard.GetComponent<SpriteRenderer>();
 
+        if (spriteRenderer != null && cardSprites.Length > 0)
+        {
+            // Randomly select a sprite from the loaded sprites
+            int randomIndex = Random.Range(0, cardSprites.Length);
+            spriteRenderer.sprite = cardSprites[randomIndex];
+        }
+        else
+        {
+            Debug.LogError("SpriteRenderer is missing or no sprites are available.");
+        }
+
+        AnimateIdle(newCard.transform);
         currentCards.Add(newCard);
     }
 
@@ -35,7 +58,6 @@ public class CardManager : MonoBehaviour
         if (currentCards.Contains(cardToUse))
         {
             Transform slot = FindSlotForCard(cardToUse);
-
             if (slot != null)
             {
                 DestroyCardWithAnimation(cardToUse, slot);
@@ -59,38 +81,28 @@ public class CardManager : MonoBehaviour
     {
         float randomScale = Random.Range(1.05f, 1.15f);
         float randomDuration = Random.Range(1f, 2f);
-
         Tween idleTween = cardTransform.DOScale(new Vector3(0.3f, 0.3f, 0.7f), 0.74f)
             .SetLoops(-1, LoopType.Yoyo)
             .SetEase(Ease.InOutSine);
-
         idleTween.Play();
     }
 
     void DestroyCardWithAnimation(GameObject cardToDestroy, Transform slot)
     {
         DOTween.Kill(cardToDestroy.transform);
-
         Sequence destroySequence = DOTween.Sequence();
-
         destroySequence.Append(cardToDestroy.transform.DOScale(0f, 0.5f).SetEase(Ease.InBack));
-
         SpriteRenderer spriteRenderer = cardToDestroy.GetComponent<SpriteRenderer>();
         if (spriteRenderer != null)
         {
             destroySequence.Join(spriteRenderer.DOFade(0f, 0.5f));
         }
-
         destroySequence.Join(cardToDestroy.transform.DOMoveY(cardToDestroy.transform.position.y + 1f, 0.5f).SetRelative());
-
         destroySequence.OnComplete(() =>
         {
             Debug.Log("Destroying object: " + cardToDestroy.name);
-
             currentCards.Remove(cardToDestroy);
-
             Destroy(cardToDestroy);
-
             SpawnRandomCard(slot);
         });
     }
